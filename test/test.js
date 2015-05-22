fcd9f4b2246785744fc1989d356d7133577af14d
grbcruz <gabriel@tagon8inc.com>
2015-05-22 16:25:37 -0300

    Add tests for the requestHandler
    Tests for each endpoint of the application.

---
+++ test/test.js
@@ -0,0 +1,27 @@
+var should = require('should');
+var requestHandler = require('../requestHandler');
+
+var response = {
+  message: "",
+  write: function(string) {
+    this.message = string;
+  }
+};
+
+describe('Testing requestHandler', function(){
+  describe('/start', function() {
+    it('should return hello string', function(){
+      requestHandler.start(response, function() {
+        response.message.should.equal('Hello Start!');
+      });
+    });
+  });
+
+  describe('/finish', function() {
+    it('should return hello string', function(){
+      requestHandler.finish(response, function() {
+        response.message.should.equal('Hello Finish!');
+      });
+    });
+  });
+});
